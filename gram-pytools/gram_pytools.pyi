3df263c8f89a4bbe20f904feb22a7189c6e8dc97
ruofeng <i@ruofengx.cn>
2025-10-30 19:38:18 +0800

    修复函数签名pyi文件错误的标记

--- gram-pytools/gram_pytools.pyi
+++ gram-pytools/gram_pytools.pyi
@@ -1,16 +1,12 @@
-from typing import Optional
-
-
-def get_mentioned(msg: str) -> Optional[tuple[set[str], set[int]]]:
+def extract_username(msg: str) -> tuple[set[str], set[int]]:
     """
     输入json字符串格式的tl::enums::Message
     
-    函数分析其内容是否包含公群链接,  
-    同时对entities进行提取(Mention, mentionName, textUrl),
+    函数分析其内容包含的所有用户名和用户ID进行提取,
     如果有则提取用户名或用户ID
 
     返回两个列表, 分别包含用户名和用户ID
-    其中, 用户名是不带@前缀的
+    用户名是不带@前缀的
     """
     ...
 
